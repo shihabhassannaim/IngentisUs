f065acb56b283bb117bc55e7e74bf81e7bcce139
shihabhassannaim <hassanshihab231@gmail.com>
2023-09-23 20:45:04 +0600

    Day-13 , 23 / 9 / 2023

---
+++ src/Components/Advertise/Advertise.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Advertise = () => {
+    return (
+        <div>
+            <h1>This is advertise .</h1>
+        </div>
+    );
+};
+
+export default Advertise;
