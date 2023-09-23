a1ef7956cbf190a356166e3c5c88faeb0c1ee25d
shihabhassannaim <hassanshihab231@gmail.com>
2023-09-23 21:15:25 +0600

    Day-13 , 23 / 9 / 2023

--- src/Components/Advertise/Advertise.js
+++ src/Components/Advertise/Advertise.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import AppDevelopment from '../../Images/appDevelopment.png'
+import WebDevelopment from '../../Images/WebDevelopment.png'
+import digitalMarketing from '../../Images/Digital campaign.png'
 
 const Advertise = () => {
     return (
         <div>
-            <h1>This is advertise .</h1>
+            <div>
+                <p>Quality features.</p>
+                <p>Meet exciting feature of services</p>
+            </div>
+            <div>
+                <div>
+                    <img src={AppDevelopment} alt="" />
+                    <h3>App Development.</h3>
+                    <p>Get your blood tests delivered at home collect a sample from the your blood tests.</p>
+                </div>
+                <div>
+                <img src={WebDevelopment} alt="" />
+                    <h3>Web Development.</h3>
+                    <p>Get your blood tests delivered at home collect a sample from the your blood tests.</p>
+                </div>
+                <div>
+                <img src={digitalMarketing} alt="" />
+                    <h3>Digital Marketing</h3>
+                    <p>Get your blood tests delivered at home collect a sample from the your blood tests.</p>
+                </div>
+            </div>
         </div>
     );
 };
