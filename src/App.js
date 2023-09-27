27c1fc1cd6075eadf48f373fe248444d83f15733
shihabhassannaim <hassanshihab231@gmail.com>
2023-09-27 18:44:00 +0600

    Day-17 , 27 / 9 / 2023

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Advertise from './Components/Advertise/Advertise';
 import CoreFeatures from './Components/Advertise/CoreFeatures';
 import CoreFeaturesAnother from './Components/Advertise/CoreFeaturesAnother';
+import MoreAbout from './Components/Advertise/MoreAbout';
 import Home from './Components/Home/Home';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Advertise />
       <CoreFeatures />
       <CoreFeaturesAnother />
+      <MoreAbout></MoreAbout>
     </div>
   );
 }
---
+++ src/Components/Advertise/MoreAbout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import moreAboutImage from '../../Images/company 1.png'
+
+const MoreAbout = () => {
+    return (
+        <div>
+            <div><img src={moreAboutImage} alt="" /></div>
+            <div>
+                <p>More About <span>Our Company !!! </span></p>
+                <p>Organize Your Project Content</p>
+                <p>Get your website ads tests delivered at let collect sample from the <br /> victory of the managments that supplies best design system <br />which guidelines ever with multiple features.</p>
+                <p>Collaborate your multiple team support easily</p>
+                <p>Build your team's knowledge base system </p>
+            </div>
+        </div>
+    );
+};
+
+export default MoreAbout;
