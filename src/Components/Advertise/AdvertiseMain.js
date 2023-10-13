e5fbf72dea1ceca3a82e7956504333d649ad7871
shihabhassannaim <hassanshihab231@gmail.com>
2023-10-13 10:55:11 +0600

    DOne

--- src/Components/Advertise/AdvertiseMain.js
+++ src/Components/Advertise/AdvertiseMain.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Advertise from './Advertise';
 import CoreFeatures from './CoreFeatures';
 import CoreFeaturesAnother from './CoreFeaturesAnother';
-import Navbar from '../Header/Navbar';
 
 const AdvertiseMain = () => {
     return (
