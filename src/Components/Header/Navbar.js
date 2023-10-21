31f25d70b03f710b8dcba0bd8b6222614e142b75
shihabhassannaim <hassanshihab231@gmail.com>
2023-10-21 22:10:39 +0600

    Day-41

--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -5,12 +5,12 @@ import Logo from '../../Images/logo.png'
 const Navbar = () => {
     return (
         <div>
-                <div className='flex text-white'>
-                <div ><img src={Logo} alt="" /></div>
-                <div className='mt-3 ml-32'><Link to='/home'>Home</Link></div>
-                <div className='mt-3 ml-14'><Link to='/advertise'>Advertise</Link></div>
-                <div className='mt-3 ml-14'><Link to='/support'>Support</Link></div>
-                <div className='mt-3 ml-14'><Link to='/contact'>Contact</Link></div>
+                <div className='flex text-white sm:'>
+                <div><img className='h-12 w-48' src={Logo} alt="" /></div>
+                <div className='mt-3 ml-32 sm:hidden ...'><Link to='/home'>Home</Link></div>
+                <div className='mt-3 ml-14 '><Link to='/advertise'>Advertise</Link></div>
+                <div className='mt-3 ml-14 '><Link to='/support'>Support</Link></div>
+                <div className='mt-3 ml-14 '><Link to='/contact'>Contact</Link></div>
                 <div className='mt-2 ml-40 '><button className='button py-1.5 px-4 rounded-3xl'>Get Started</button></div>
             </div>
         </div>
